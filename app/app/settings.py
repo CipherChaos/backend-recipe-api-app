5e039ef965086a8a96bfb484a47f668801834d00
ariyan <ariyanbolandi3@gmail.com>
2025-11-28 18:14:31 +0330

    chore(settings): configure static/media urls and refine SPECTACULAR SETTINGS

--- app/app/settings.py
+++ app/app/settings.py
@@ -124,8 +124,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
+
+STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = '/vol/web/media'
 
 AUTH_USER_MODEL = 'core.User'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SPECTACULAR_SETTINGS = {
+    'COMPONENT_SPLIT_REQUEST': True,
+}
